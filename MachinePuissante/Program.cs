ff797dbd7fdf061fe79d28271697e296683a9263
moreirado <daniel.moreiradossantosrpn.ch>
2017-11-10 17:09:58 +0100

    new file visual studio

---
+++ MachinePuissante/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachinePuissante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
